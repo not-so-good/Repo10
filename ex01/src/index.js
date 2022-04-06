70bff8ab69980c4e97a5151a9107aaba7d196c40
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-06 19:41:54 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+//Only change code below this line
+var myPet = {
+    "species": "Jack Russell",
+    "name": "Kimmi",
+    "legs": 4,
+    "friends": ["Vedran", "Sanjin"]
+}
+function myFunction(myObj){
+    return myObj;
+}
+  
+//Only change code above this line 
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
